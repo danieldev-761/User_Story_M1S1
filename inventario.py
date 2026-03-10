d25fa6251c9cd6862a25c587ebe34df82111c248
IsaacRG5 <isaacdavid1127@gmail.com>
2026-03-10 09:39:24 -0500

    En proceso

--- inventario.py
+++ inventario.py
@@ -1,1 +1,33 @@
+print("""
+---------------Registro de ventas--------------
+        1. Nuevo producto 
+        2. Salir
+
+""")
+opcion = input("Que opcion deseas realizar? ")
+
+while True:
+        if opcion == "1":
+                        nombres = input("Nombre del producto: ").strip().capitalize()
+                        if nombres == "" or nombres.isnumeric():
+                                print("El nombre no puede estar vacío o no puede ser solo números..")
+
+                        else:
+                                try:
+                                        precio =float(input("Digite el precio del producto: "))
+                                        
+                                        if precio < 0 :
+                                                print("El valor no puede ser menor a 0 o por de bajo de 0 ")
+                                                continue
+
+                                        cantidad = int(input("Ingrese la cantidad del producto: "))
+
+                                        if cantidad > 0:
+                                                print("La cantidad no puede ser menor a 0 o 0")
+                                except ValueError:
+                                        print("ERROR: No se acepta ese tipo de valor")
+
+
+
+
 
