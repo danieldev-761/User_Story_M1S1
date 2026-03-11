370e990efad366c317a565bf2698107dbf770fad
langel280311-byte <langel280311@gmail.com>
2026-03-11 10:44:55 -0500

    codigo

--- inventario.py
+++ inventario.py
@@ -1,1 +1,17 @@
+# costo total
 
+precio= float(input("ingrese el precio del producto: "))
+cantidad= int(input("ingrese la cantidad del producto: "))
+
+#validacion 
+if precio >= 0 and cantidad >= 0:
+    costo_total= precio * cantidad
+    print("el costo total es: ", costo_total)
+else:
+    print("error precio o cantidad invalida")
+
+# resultado
+print("----resultado inventario----")
+print("precio: ", precio)
+print("cantidad: ", cantidad)
+print("costo total: ", costo_total)
